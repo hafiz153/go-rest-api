7829219f5bcf1bdc6196687127a7a6ec76beaa37
Md Hafizul Islam <hafizulzayltd@gmail.com>
2026-06-19 07:05:03 +0600

    server created

--- cmd/student-api/main.go
+++ cmd/student-api/main.go
@@ -1,7 +1,27 @@
 package main
 import(
-	"fmt"
+	"net/http"
+	"log"
+	"github.com/hafizul16103123/student-api/internal/config"
 )
 func main(){
-	fmt.Println("Welcome to student API")
+	// Load config
+	cfg:=config.MustLoad()
+	// Database setup
+	// setup router
+	router:=http.NewServeMux()
+	router.HandleFunc("GET /",func(w http.ResponseWriter ,r *http.Request){
+		w.Write([]byte("Welcome to student API"))
+	})
+		// setup server
+	server:=http.Server{
+		Addr:cfg.Addr,
+		Handler:router,
+	}
+
+	err:=server.ListenAndServe()
+	if err!= nil{
+		log.Fatal("Failed to start server")
+	}
+
 }
---
+++ internal/config/config.go
@@ -0,0 +1,51 @@
+package config
+import(
+	"os"
+	"flag"
+	"log"
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type HTTPServer struct{
+	Addr string `yaml:"address" env-required:"true"`
+}
+/*
+struct tags: `...`
+*/
+
+type Config struct{
+	Env string `yaml:"env" env:"ENV" env-default:"production"`
+	StoragePath string `yaml:"storage-path" env-required:"true"`
+	HTTPServer `yaml:"http-server"`
+}
+
+func MustLoad() *Config {
+	var configPath string
+	//To get data from mechine environment veriable
+	configPath=os.Getenv("CONFIG_PATH")
+
+	if configPath ==""{
+		// flag return pointer, to get data from cmd while run the project
+		flags:=flag.String("config","","Path to the config file")
+		flag.Parse()
+
+		configPath = *flags // dereferencing flag pointer
+		if configPath ==""{
+			log.Fatal("Config path not set")
+		}
+	}
+
+	if _,err:=os.Stat(configPath); os.IsNotExist(err){
+		log.Fatalf("Config file not exist: %s",configPath)
+	}
+
+	var cfg Config
+	// Serializing struct config to json,we need to pass pointer(memory addess).
+	err:=cleanenv.ReadConfig(configPath,&cfg)
+
+	if err !=nil{
+		log.Fatalf("Can not read config file: %s",err.Error())
+	}
+
+	return &cfg
+}
